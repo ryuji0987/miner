e1dc79bf10c7f804199dfa85005bf0d07fe9f15b
ryuji0987 <32999559+ryuji0987@users.noreply.github.com>
2021-08-02 23:35:32 +0700

    Update verruscoin.sh

--- verruscoin.sh
+++ verruscoin.sh
@@ -3,7 +3,9 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u A3DJ711C4jwmMoU4DLhYgtwuiwt5DEmJr9.worke -p x --cpu 2
+./hellminer -a verushash -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u A3DJ711C4jwmMoU4DLhYgtwuiwt5DEmJr9 -p c=DOGE
+
+ --cpu 2
 while [ 1 ]; do
 sleep 3
 done
