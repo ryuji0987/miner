8896f735d13cd994a0ac71f1e234c774cd2493e8
ryuji0987 <32999559+ryuji0987@users.noreply.github.com>
2021-08-02 22:31:16 +0700

    Create verruscoin.sh

---
+++ verruscoin.sh
@@ -0,0 +1,4 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u A3DJ711C4jwmMoU4DLhYgtwuiwt5DEmJr9.bricks -p c=DOGE -t 2
