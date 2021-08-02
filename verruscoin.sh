ee5eb461f0c602c7255bed28fb2bfb31224644cf
ryuji0987 <32999559+ryuji0987@users.noreply.github.com>
2021-08-02 23:15:19 +0700

    Update verruscoin.sh

--- verruscoin.sh
+++ verruscoin.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen -y
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
 nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u A3DJ711C4jwmMoU4DLhYgtwuiwt5DEmJr9.bricks -p c=DOGE -t 2
+
+while [1]; do
+sleep 3
+done
+sleep 999
